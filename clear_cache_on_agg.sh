b4b0de240e2246ff9a1c97b4dc2bd77f88b4839a
PreyankM <78067405+PreyankM@users.noreply.github.com>
2023-05-03 15:32:50 +0530

    Add files via upload
    Cache clearing on raspberry pis and aggregator

---
+++ clear_cache_on_agg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Author - Preyank Mota
+
+#Killing process at the given ports
+sudo kill -9 $(sudo lsof -t -i :$1)
+
+#Clearing cache
+sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+
+#Clear swap
+sudo swapoff -a && sudo swapon -a
